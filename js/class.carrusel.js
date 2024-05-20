96371802c29ab33a39f0bd9eb042938287f7c53c
albem84 <103131088+albem84@users.noreply.github.com>
2024-05-19 21:14:21 -0300

    solucionado problemas en area de contactos y servicios

--- js/class.carrusel.js
+++ js/class.carrusel.js
@@ -1,44 +1,44 @@
 const izquierda = document.querySelector(".boton_izquierdo"),
-    derecha = document.querySelector(".boton_derecho"),
-    slider = document.querySelector("#frame"),
-    sliderSection = document.querySelectorAll(".slider_section");
-
-izquierda.addEventListener("click", e => moverIzquierda())
-derecha.addEventListener("click", e => moverDerecha())
-
-setInterval(() => {
-    moverDerecha()
-}, 5000);
+  derecha = document.querySelector(".boton_derecho"),
+  slider = document.querySelector("#frame"),
+  sliderSection = document.querySelectorAll(".slider_section");
+console.table(derecha);
+izquierda.addEventListener("click", (e) => moverIzquierda());
+derecha.addEventListener("click", (e) => moverDerecha());
 
 let operacion = 0,
-    contador = 0,
-    ancho = 100 / sliderSection.length;
+  contador = 0,
+  ancho = 100 / sliderSection.length;
 
 function moverDerecha() {
-    if (contador >= sliderSection.length - 1) {
-        contador = 0;
-        operacion = 0;
-        slider.style.transform = `translate(-${operacion}%)`;
-        slider.style.transition = "none";
-        return;
-    }
-    contador++;
-    operacion = operacion + ancho;
+  alert();
+  if (contador >= sliderSection.length - 1) {
+    contador = 0;
+    operacion = 0;
     slider.style.transform = `translate(-${operacion}%)`;
-    slider.style.transition = "all ease-in 2s"
-
+    slider.style.transition = "none";
+    return;
+  }
+  contador++;
+  operacion = operacion + ancho;
+  slider.style.transform = `translate(-${operacion}%)`;
+  slider.style.transition = "all ease-in 2s";
 }
 
 function moverIzquierda() {
-    contador--;
-    if (contador < 0) {
-        contador = sliderSection.length - 1;
-        operacion = ancho * (sliderSection.length - 1)
-        slider.style.transform = `translate(-${operacion}%)`;
-        slider.style.transition = "none";
-        return;
-    }
-    operacion = operacion - ancho;
+  contador--;
+  if (contador < 0) {
+    contador = sliderSection.length - 1;
+    operacion = ancho * (sliderSection.length - 1);
     slider.style.transform = `translate(-${operacion}%)`;
-    slider.style.transition = "all ease-out 2s"
-} 
+    slider.style.transition = "none";
+    return;
+  }
+  operacion = operacion - ancho;
+  slider.style.transform = `translate(-${operacion}%)`;
+  slider.style.transition = "all ease-out 2s";
+}
+
+setInterval(() => {
+  moverDerecha();
+}, 5000);
